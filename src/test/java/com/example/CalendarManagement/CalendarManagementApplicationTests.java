ec19226dff0e5d04f68b59a4d044002ed3081306
Amar Sankannavar <amarsankannavar@CLPT2156.local>
2025-02-11 11:20:22 +0530

    Initial commit

---
+++ src/test/java/com/example/CalendarManagement/CalendarManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CalendarManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
